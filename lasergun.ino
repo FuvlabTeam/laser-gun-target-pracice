1d5d78e392c9dd7e07504b9c60d6d6881d273ded
Fuvlab team <contact@catchu.org>
2016-10-15 15:22:21 +0200

    Add files via upload

---
+++ lasergun.ino
@@ -0,0 +1,160 @@
+#define ledp 10 //LED - need pwm
+#define buzzp 11 //BUZZER - need pwm
+#define laserp 9
+#define pb1 4 //trigger
+#define pb2 2 //debug
+#define pot A0
+
+int laser, shotd=200, shotsnd;
+int charge, recharge, mincharge=50, maxcharge=255, chargeout;
+long shott, t;
+int pb1dwn, d, dmin=20, dmax=100;
+int CHEAT, deb = 150;
+
+void setup() {    
+  //Serial.begin(9600);  
+  pinMode(ledp, OUTPUT);
+  pinMode(buzzp, OUTPUT);
+  pinMode(laserp, OUTPUT);
+  pinMode(pb1, INPUT_PULLUP);
+  pinMode(pb2, INPUT_PULLUP);
+  pinMode(pot, INPUT);
+  digitalWrite(ledp, HIGH);  
+  laser = LOW;
+  shotsnd = 0;
+  charge = maxcharge;
+  recharge = 0;
+  chargeout = 0;
+  pb1dwn = false;
+  
+  if (digitalRead(pb1)==LOW) { 
+    tone(buzzp,100,2000);
+    for (int i=0;i<10;i++) {
+      tone(buzzp,100);
+      analogWrite(ledp, 255);
+      delay(200);
+      noTone(buzzp);
+      analogWrite(ledp, 0);
+      delay(50); 
+    }
+    CHEAT = true;
+  } 
+  else {
+    CHEAT = false;
+    bootsequence();
+  }
+}
+
+void loop() {
+  //d = map(analogRead(pot),0,1023,dmin,dmax);
+  
+  if (CHEAT) charge = maxcharge;
+  
+  if (charge<maxcharge && !chargeout) {
+    recharge++;
+    if (recharge>200) {
+      recharge=0;
+      charge++;
+    }
+  }
+  
+  // led lighting
+  if (charge>mincharge) analogWrite(ledp, charge);
+    else analogWrite(ledp, LOW); 
+  
+  // shot sound
+  if (shotsnd > (1000)) {
+    tone(buzzp,shotsnd);
+    shotsnd-=4;
+  } else noTone(buzzp);
+
+  t = millis();  
+  
+  // turn off laser if on for more than shotd
+  if (laser && (t-shott>shotd)) {
+    laser = LOW;
+    digitalWrite(laserp,laser);
+  }
+  
+  // trigger debounce
+  if (digitalRead(pb1)==HIGH && t-shott > deb) pb1dwn = false;
+  
+  // 
+  if (chargeout && t-shott>3000) {
+    chargeout = 0;
+    charge = mincharge;
+  }
+  
+  //shot triggered
+  if (digitalRead(pb1)==LOW && !pb1dwn) {
+    pb1dwn = true;
+    //debug();
+    // enough juice to shoot
+    if (charge>mincharge) {
+      shott = millis();
+      laser = HIGH;
+      digitalWrite(laserp,laser);
+      shotsnd = 2500;
+      charge -= mincharge;
+      if (charge<mincharge) chargeout = 1;
+    } else { // not enough juice
+      tone(buzzp,50,200);
+      delay(200);
+    }
+    //debug();
+  }
+  
+  if (digitalRead(pb2) == LOW) {
+    debug();
+    charge=maxcharge;
+  }
+}
+
+void bootsequence() {
+  int i;
+  for (i=0;i<4;i++) {
+    tone(buzzp,1500);
+    analogWrite(ledp, 255);
+    delay(50);
+    noTone(buzzp);
+    analogWrite(ledp, 0);
+    delay(1000);
+  }
+  for (i=20;i<800;i++) {
+    tone(buzzp,i);
+    analogWrite(ledp, int(i/10));
+    delay(5);
+  }
+  delay(200);
+  for (i=0;i<10;i++) {
+    tone(buzzp,800);
+    analogWrite(ledp, 255);
+    delay(50);
+    noTone(buzzp);
+    analogWrite(ledp, 0);
+    delay(50);
+  }
+  tone(buzzp,800);
+  digitalWrite(laserp,HIGH);
+  analogWrite(ledp, 255);
+  delay(1000);
+  noTone(buzzp);
+  digitalWrite(laserp,LOW);
+}
+
+void flashled(int n, int d) {
+  for (int i=0;i<n;i++) {
+    digitalWrite(ledp, LOW); 
+    delay(d);
+    digitalWrite(ledp, HIGH);
+    delay(d);
+  }
+}
+
+void debug() {
+  //Serial.print(analogRead(pot)); Serial.print("  "); 
+  //Serial.print(d); Serial.print("  ");
+  Serial.println(charge); //Serial.print("  "); 
+  //Serial.print(mode); Serial.print("  ");
+  //Serial.println(state);
+}
---
+++ lasertarget.ino
@@ -0,0 +1,124 @@
+#define tgtpin A0
+#define buzzpin 10
+#define ledpin A2
+
+// mapping 7-segment
+// 10 9 8 7 6
+//  G F + A B
+//  5 4   3 2 ardu
+//
+//  9 8   7 6 ardu
+//  E D + C DP
+//  1 2 + 4 5
+
+byte wiremap[7] = {3,2,7,8,9,4,5};
+byte digits[10][7] = { { 1,1,1,1,1,1,0 }, { 0,1,1,0,0,0,0 }, { 1,1,0,1,1,0,1 }, 
+{ 1,1,1,1,0,0,1 }, { 0,1,1,0,0,1,1 }, { 1,0,1,1,0,1,1 }, { 1,0,1,1,1,1,1 }, 
+{ 1,1,1,0,0,0,0 }, { 1,1,1,1,1,1,1 }, { 1,1,1,0,0,1,1 }};
+
+int pb, score, count,  incr;
+int target, baseline, gameon;
+long startt, t;
+
+void setup() {  
+  //Serial.begin(115200);
+  for (int i=0;i<8;i++) pinMode(wiremap[i], OUTPUT);   
+  pinMode(buzzpin,OUTPUT);
+  pinMode(ledpin,OUTPUT);
+  pinMode(tgtpin,INPUT);
+  boot();
+}
+
+void loop() {
+  int i;
+  t = millis();
+  if (t-startt>incr && gameon) {
+    startt = t;
+    count++;
+    if (count==5) incr = 1000;
+    if (count==15) {
+      tone(buzzpin,150,2000);
+      flashDigit(score,5,200,200,0);
+      writeDigit(score);
+      gameon = false; incr=10000; startt=millis();
+    } else tone(buzzpin,2000,20);
+  }
+  
+  if (gameon) {
+    target = analogRead(tgtpin);
+    //Serial.println(target);
+    if (target < baseline*0.8) {
+      score++;
+      if (score==10) {
+        gameon = false; incr=10000; startt=millis();
+        digitalWrite(ledpin,HIGH);
+        for (i=200;i<880;i++) tone(buzzpin,i);
+        flashDigit(0,15,100,100, 880);
+        digitalWrite(ledpin,LOW);
+        flashDigit(0,10,500,500, 0);
+      } else {
+        digitalWrite(ledpin,HIGH);
+        flashDigit(score, 6, 80, 80, 150);
+        digitalWrite(ledpin,LOW);
+        writeDigit(score);
+      }
+    }
+  } else { // !gameon
+    t = millis();
+    if (t-startt>incr) boot();
+  }
+}
+
+void boot() {
+  int i;
+  long tpo;
+  writeDot(0);
+  writeDigit(0);
+  score = 0;
+  tpo = 0;
+  for (i=0;i<50;i++) {
+    tpo += analogRead(tgtpin);
+    clearDigit();
+    noTone(buzzpin);
+    delay(20);
+    writeDigit(0);
+    tone(buzzpin,1000);
+    delay(20);
+  }
+  baseline = tpo/50;
+  //Serial.println(baseline);
+  delay(1000);
+  noTone(buzzpin);
+  gameon = true;
+  count = 0;
+  incr = 10000;
+  startt = millis();
+}
+
+void writeDot(byte dot) {
+  digitalWrite(6, !dot);
+}
+    
+void writeDigit(int d) {
+  for (int i=0; i<7; i++) {
+    digitalWrite(wiremap[i], !digits[d][i]);
+  }
+}
+
+void clearDigit() {
+  for (int i=0; i<8; i++) {
+    digitalWrite(wiremap[i], 1);
+  }
+}
+
+void flashDigit(int d, int n, int on, int off, int snd) { 
+  for (int i=0; i<n; i++) {
+    writeDigit(d);
+    if (snd) tone(buzzpin,snd);
+    delay(on);
+    clearDigit();
+    if (snd) noTone(buzzpin);
+    delay(off);
+  }
+}
+
